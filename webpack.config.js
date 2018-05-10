1088372c1f6248f020685a7994bc544cba8999c8
Michael joseph <michael@Mac-mini-de-Michael.local>
2018-05-10 09:28:34 +0200

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/app.ts',
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
